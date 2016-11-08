5e415c36fb61ea6b75df40665d25c26f8d20cae4
BinouX <liebaertb@gmail.com>
2016-11-08 13:59:49 +0100

    ajout imageProfil/suppression/nbResultat

--- Form_Angular/app/app.js
+++ Form_Angular/app/app.js
@@ -6,30 +6,33 @@ appTest.controller("testController", function($scope) {
             name: '',
             nom: '',
             age: '',
-            work: ''
+            work: '',
+            img: 'mystere'
         };
         $scope.clients = [{
             name: 'Binoux',
             nom: 'Liebaert',
             age: 22,
-            work: 'Pictime'
+            work: 'Pictime',
+            img: 'joker'
         }, {
             name: 'Alexis',
             nom: 'Verquin',
             age: 27,
-            work: 'Coreye'
-
+            work: 'Coreye',
+            img: 'batman'
         }, {
             name: 'Valentin',
             nom: 'Liebaert',
             age: 20,
-            work: 'IIID'
-
+            work: 'IIID',
+            img: 'pingouin'
         }, {
             name: 'Lorinne',
             nom: 'Timothee',
             age: 25,
-            work: 'Capgemini'
+            work: 'Capgemini',
+            img: 'quinn'
         }];
         var youngest = $scope.clients[0].age;
         var getYoungest = function() {
@@ -99,10 +102,10 @@ appTest.controller("testController", function($scope) {
 
         $scope.isDisable = function() {
             if (typeof $scope.client.name === "undefined" &&
-                  typeof $scope.client.nom === "undefined" &&
-                    typeof $scope.client.work === "undefined" &&
-                      $scope.client.age === '' || isNaN($scope.client.age)
-                ) {
+                typeof $scope.client.nom === "undefined" &&
+                typeof $scope.client.work === "undefined" &&
+                $scope.client.age === '' || isNaN($scope.client.age)
+            ) {
                 return isDisable;
             }
 
@@ -123,7 +126,30 @@ appTest.controller("testController", function($scope) {
                 isDisable = false;
             }
             return isDisable;
-        }
+        };
+
+        var showUpdateClient=false;
+        $scope.showUpdateClient = function(){
+          showUpdateClient = true;
+        };
+
+        $scope.getShowUpdateClient = function(){
+            return showUpdateClient;
+        };
+
+        $scope.updateClient = function(name) {
+
+        };
+
+        $scope.removeClient = function(name) {
+            var seekClients = 0
+            $scope.clients.forEach(function(client) {
+                if (client.name === name.name && client.nom === name.nom) {
+                    $scope.clients.splice(seekClients, 1);
+                }
+                seekClients++;
+            })
+        };
     })
     .controller("menuController", function($scope) {
         $scope.menus = [{
