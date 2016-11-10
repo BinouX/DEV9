247c2e80e4f642761d4e15ac9a6c6c92e80b1a90
BinouX <liebaertb@gmail.com>
2016-11-10 12:33:04 +0100

    angular 10/11/2016 12:33

--- Form_Angular/app/directives/clientDirective.js
+++ Form_Angular/app/directives/clientDirective.js
@@ -1,11 +1,16 @@
 angular.module("testApp")
-  .directive("client", function(){
-    return  {
-        restrict: "E",
-        templateUrl: "./app/directives/templates/clientTemplate.html",
-        scope: {
-          message: "@",
-          data: '='
-        }
-    };
-  })
+    .directive("client", function() {
+        return {
+            restrict: "E",
+            templateUrl: "./app/directives/templates/clientTemplate.html",
+            scope: {
+                message: "@",
+                data: '='
+            },
+            link: function(scope, element, attribut) {
+                scope.displayMessage = function(bongour){
+                  console.log(bongour);
+                }
+            }
+        };
+    })
